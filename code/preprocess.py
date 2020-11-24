7cc0021dd3c152f40e7e91333767638f59933ce8
Xingyan Wang <xingyan_wang1@brown.edu>
2020-11-24 03:46:38 -0500

    Update preprocess.py

--- code/preprocess.py
+++ code/preprocess.py
@@ -3,16 +3,21 @@ import numpy as np
 from music21 import *
 import glob
 
-def get_notes(file_name):
-    """ Get all the notes and chords from the midi files in the ./midi_songs directory """
+def get_notes():
+    """ 
+    Get all the notes and chords from the midi files in the ./data directory
+    
+    :return notes: 1-d list with all notes and chords represented as strings
+    """
     notes = []
 
-    for file in glob.glob(file_name):
+    for file in glob.glob('../../MusicBot-by-Producer-404/data/*.mid'):
         midi = converter.parse(file)
-        # midi = music21.converter.parse("../../MusicBot-by-Producer-404/midi_songs/cosmo.mid")
+        # midi = music21.converter.parse("../../MusicBot-by-Producer-404/data/cosmo.mid")
     
         print("Parsing %s" % file)
-
+        
+        # Only parse notes and chords in first track, which usually contains the main melody
         notes_to_parse = midi.parts[0].flat.notes     
 
         for element in notes_to_parse:
@@ -21,8 +26,31 @@ def get_notes(file_name):
             elif isinstance(element, chord.Chord):
                 notes.append('.'.join(str(n) for n in element.normalOrder))
                 
-    # 
+    # prevent overfitting, need to do more research on this
     # with open('data/notes', 'wb') as filepath:
     #     music21.pickle.dump(notes, filepath)
 
     return notes
+
+def get_data():
+    """
+    Read the data files and extract notes and chords sequence from the files.
+    Create a note dictionary that maps all the unique notes and chords from 
+    data as keys to a unique integer value.
+    Then vectorize train data based on note dictionary.
+
+    :return: Tuple of train (1-d list with training notes in id form), 
+             vocabulary (dict containg note->index mapping)
+    """
+    notes = get_notes()
+    
+    # mapping notes to ids
+    all_notes = sorted(set(notes))
+    notes_dict = {note: i for i, note in enumerate(all_notes)}
+    
+    # process train data according to notes_dict
+    train = []
+    for note in notes:
+        train.append(notes_dict[note])
+    
+    return train, notes_dict
