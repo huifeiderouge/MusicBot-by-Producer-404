916b768bab7e04eece658cc3a8433074427f13bb
Xingyan Wang <xingyan_wang1@brown.edu>
2020-12-03 23:08:01 -0500

    Add train() and main() in model_RNN_seq.py

--- code/model_RNN_seq.py
+++ code/model_RNN_seq.py
@@ -21,3 +21,24 @@ def build_model(vocab_size, embedding_size):
     model.compile(loss='sparse_categorical_crossentropy', optimizer='Adam')
     
     return model
+
+def train(model, inputs, lables):
+    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    callbacks_list = [checkpoint]
+
+    model.fit(inputs, lables, epochs=50, batch_size=128, callbacks=callbacks_list)
+    
+def main():
+    train_data, notes_dict = get_data()
+    model = build_model(len(notes_dict), 128)
+    train(model, train_data[:-1], train_data[1:])
+    
+if __name__ == '__main__':
+    main()
